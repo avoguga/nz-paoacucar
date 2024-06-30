0d7966249a119794dd2a0b13f3113a1cbae60a9e
Gustavo <avoguga@gmail.com>
2024-06-30 14:20:21 -0300

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
+import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), crossOriginIsolation()],
     resolve: {
       alias: [
         { find: 'main', replacement: path.resolve(__dirname, 'src/main') },
